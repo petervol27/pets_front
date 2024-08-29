db79fb099d254e74479e4cad5187d997a0d9e6fa
Pietro <petervol35@gmail.com>
2024-08-29 20:17:50 +0300

    added call btn

--- general.js
+++ general.js
@@ -1,3 +1,20 @@
 // Production URL:  https://pets-2hb2.onrender.com/pets/
 // Development URL: http://127.0.0.1:5000
 const hostName = 'https://pets-2hb2.onrender.com/pets/';
+
+const deletePet = async (id) => {
+  const response = await axios.delete(`${hostName$}${id}/`);
+  alert(response.data.response);
+  getPets();
+};
+const goToEdit = (id) => {
+  window.location.href = `https://pets-front.onrender.com/editPet?id=${id}`;
+};
+const petCall = (type) => {
+  const animalCry = type.toLowerCase();
+  if (animalCry == 'cat') {
+    alert('Meow!!!');
+  } else {
+    alert('WOOF!!!!');
+  }
+};
